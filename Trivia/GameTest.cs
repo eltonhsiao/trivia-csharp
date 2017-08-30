6b3482b6072c77e1e5b4b5688da061b16c7449be
Stanly Lau <stanly@odd-e.com>
2017-08-30 14:12:30 +0800

    First import

---
+++ Trivia/GameTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+
+namespace Trivia
+{
+    [TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    class GameTest
+    {
+        [Test]
+        public void TwoPlayerWithInOutPenaltyBox()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var game = new Game();
+            game.add("John");
+            game.add("Mary");
+            
+            game.roll(5);
+            game.wasCorrectlyAnswered();
+
+            // get into penalty box
+            game.roll(4);
+            game.wrongAnswer();
+
+
+            game.roll(4);
+            game.wasCorrectlyAnswered();
+
+            // not getting out of penalty box
+            game.roll(2);
+            game.wasCorrectlyAnswered();
+
+
+            game.roll(6);
+            game.wasCorrectlyAnswered();
+
+            // odd number gets out of penalty box
+            game.roll(9);
+            game.wasCorrectlyAnswered();
+
+
+            game.roll(2);
+            game.wasCorrectlyAnswered();
+
+            game.roll(1);
+            game.wasCorrectlyAnswered();
+
+            Approvals.Verify(writer.ToString());
+        }
+    }
+}
